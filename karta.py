51dad9102f084baa09c9930bae5a1a9b5251c8f6
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2023-10-04 21:55:41 +0300

    fix

---
+++ karta.py
@@ -0,0 +1,6 @@
+from player import Player
+
+class karta:
+    def __init__(self, cards, position_of_players):
+        self.cards = []
+        self.positions_of_players = [Player.position]
---
+++ player.py
@@ -0,0 +1,30 @@
+class Player:
+    def __init__(self, name, password, cards, id, money, busters, position):
+        self.name = str
+        self.password = str
+        self.id = int
+        self.money = int
+        self.position = int
+        self.cards = []
+        self.busters = []
+
+    def hod():
+        pass
+    def bust():
+        pass
+    def change_streets():
+        pass
+    def pay():
+        pass
+    def take_money():
+        pass
+    def take_buster():
+        pass
+    def autorizatia():
+        pass
+    def invite_to_game():
+        pass
+    def craete_the_game():
+        pass
+    def leave_the_game():
+        pass
